8af559288a8c11fab80ab4381c3be64662ba628a
irsol <irinywka@gmail.com>
2018-06-14 17:42:26 +0200

    Add new quizzes to basic sql topic

--- basic_sql_lesson28/quizzes.sql
+++ basic_sql_lesson28/quizzes.sql
@@ -118,3 +118,17 @@ WHERE name LIKE '%one%';
 SELECT *
 FROM accounts
 WHERE name LIKE '%s%';
+
+## IN
+/*Use the accounts table to find the account name, primary_poc, and sales_rep_id for Walmart, Target, and Nordstrom.*/
+
+SELECT name, primary_poc, sales_rep_id
+FROM accounts
+WHERE name IN ('Walmart', 'Target', 'Nordstrom');
+
+/*Use the web_events table to find all information regarding individuals who were contacted via the channel of organic or adwords.*/
+
+SELECT channel
+FROM web_events
+WHERE channel IN ('organic', 'adwords');
+
