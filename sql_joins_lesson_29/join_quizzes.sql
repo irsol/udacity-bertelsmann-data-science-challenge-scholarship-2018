2acea6693df81c8d9eba24fb447403f5ebd01c78
irsol <irinywka@gmail.com>
2018-07-07 12:02:47 +0200

    Added 3 quizz solution.

--- sql_joins_lesson_29/join_quizzes.sql
+++ sql_joins_lesson_29/join_quizzes.sql
@@ -47,9 +47,37 @@ JOIN region ON sales_reps.region_id = region.id;
 
 ## JOINs and Filtering. Quiz: Last Check
 
-/*1.*/
+/*1.Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for the Midwest region. 
+Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically
+(A-Z) according to account name.*/
 
-/**/
-/**/
-/**/
+SELECT region.name AS Region, sales_reps.name AS SalesRepName, accounts.name AS AcountName
+FROM accounts 
+JOIN sales_reps 
+ON accounts.sales_rep_id = sales_reps.id
+JOIN region ON sales_reps.region_id = region.id
+WHERE region.name = '%Midwest%';
+
+/*2.Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for accounts where the sales rep has a first
+name starting with S and in the Midwest region. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the
+accounts alphabetically (A-Z) according to account name.*/
+
+SELECT region.name, sales_reps.name AS SalesRepName, accounts.name AS AcountName
+FROM accounts 
+JOIN sales_reps 
+ON accounts.sales_rep_id = sales_reps.id
+JOIN region ON sales_reps.region_id = region.id
+WHERE region.name = '%Midwest%' and sales_reps.name = 'S%';
+
+/*3.Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for accounts where the sales rep has a last name
+starting with K and in the Midwest region. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts
+alphabetically (A-Z) according to account name.*/
+
+SELECT region.name, sales_reps.name AS SalesRepName, accounts.name AS AcountName
+FROM accounts 
+JOIN sales_reps 
+ON accounts.sales_rep_id = sales_reps.id
+JOIN region ON sales_reps.region_id = region.id
+WHERE region.name = '%Midwest%' and sales_reps.name = 'K%';
+
 /**/
