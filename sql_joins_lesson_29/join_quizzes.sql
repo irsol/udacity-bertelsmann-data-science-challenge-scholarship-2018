1b65a2485fe41e86b0caf0d0980ed22ace0cde36
irsol <irinywka@gmail.com>
2018-07-07 12:44:05 +0200

    Added solution to thr quizz number 6.

--- sql_joins_lesson_29/join_quizzes.sql
+++ sql_joins_lesson_29/join_quizzes.sql
@@ -112,5 +112,15 @@ WHERE orders.standard_qty = 100 and poster_qty = 50 ORDER BY unit_price ASC;
 the results if the standard order quantity exceeds 100 and the poster order quantity exceeds 50. Your final table should have 3 columns: region name, account name, and unit price.
 Sort for the largest unit price first. In order to avoid a division by zero error, adding .01 to the denominator here is helpful (total_amt_usd/(total+0.01). */
 
+SELECT region.name, accounts.name AS AcountName, orders.total_amt_usd/(orders.total + 0.01) AS unit_price
+FROM orders 
+JOIN accounts
+ON orders.account_id = accounts.id
+JOIN sales_reps 
+ON accounts.sales_rep_id = sales_reps.id
+JOIN region 
+ON sales_reps.region_id = region.id
+WHERE orders.standard_qty = 100 and poster_qty = 50 ORDER BY unit_price DESC;
 
+/*7.*/
 
