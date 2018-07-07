206c4ceef9d28f29d45d8f98f47f93e4339170f8
irsol <irinywka@gmail.com>
2018-07-07 12:39:47 +0200

    Added solution to the quizz number 5.

--- sql_joins_lesson_29/join_quizzes.sql
+++ sql_joins_lesson_29/join_quizzes.sql
@@ -94,4 +94,18 @@ JOIN region
 ON sales_reps.region_id = region.id
 WHERE orders.standard_qty = 100;
 
-/*5.*/
+/*5.Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. However, you should only provide
+the results if the standard order quantity exceeds 100 and the poster order quantity exceeds 50. Your final table should have 3 columns: region name, account name, and unit price.
+Sort for the smallest unit price first. In order to avoid a division by zero error, adding .01 to the denominator here is helpful (total_amt_usd/(total+0.01).*/
+
+SELECT region.name, accounts.name AS AcountName, orders.total_amt_usd/(orders.total + 0.01) AS unit_price
+FROM orders 
+JOIN accounts
+ON orders.account_id = accounts.id
+JOIN sales_reps 
+ON accounts.sales_rep_id = sales_reps.id
+JOIN region 
+ON sales_reps.region_id = region.id
+WHERE orders.standard_qty = 100 and poster_qty = 50 ORDER BY unit_price ASC;
+
+/*6.*/
