6e6802a43a2dd30b3bc68f850ae4b9e3d016ff93
irsol <irinywka@gmail.com>
2018-07-08 18:21:26 +0200

    Added solution to the Quiz: Guess my number.

--- control_flow_lesson_25/control_flow_practice.py
+++ control_flow_lesson_25/control_flow_practice.py
@@ -15,3 +15,24 @@ else:
     result = "Congratulations! You won a penguin!"
 
 print(result)
+
+
+# Quiz: Guess My Number
+
+# You decide you want to play a game where you are hiding
+# a number from someone.  Store this number in a variable
+# called 'answer'.  Another user provides a number called
+# 'guess'.  By comparing guess to answer, you inform the user
+# if their guess is too high or too low.
+
+answer = 10  # provide answer
+guess = 5  # provide guess
+
+if guess < answer:
+    result = "Oops!  Your guess was too low."
+elif guess > answer:
+    result = "Oops!  Your guess was too high."
+elif guess == answer:
+    result = "Nice!  Your guess matched the answer!"
+
+print(result)
