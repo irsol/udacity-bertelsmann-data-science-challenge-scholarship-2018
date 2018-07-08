d10e56a5339e3fa1f40323221011f1df88837214
irsol <irinywka@gmail.com>
2018-07-08 18:32:03 +0200

    Added solution to the Quiz: tax purchase.

--- control_flow_lesson_25/control_flow_practice.py
+++ control_flow_lesson_25/control_flow_practice.py
@@ -36,3 +36,33 @@ elif guess == answer:
     result = "Nice!  Your guess matched the answer!"
 
 print(result)
+
+
+# Quiz: Tax Purchase
+
+# Depending on where an individual is from we need to tax them
+# appropriately.  The states of CA, MN, and
+# NY have taxes of 7.5%, 9.5%, and 8.9% respectively.
+# Use this information to take the amount of a purchase and
+# the corresponding state to assure that they are taxed by the right
+# amount.
+
+state = 'CA'  # Either CA, MN, or NY
+purchase_amount = 21  # amount of purchase
+
+if state == 'CA':
+    tax_amount = .075
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+elif state == 'MN':
+    tax_amount = .095
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+elif state == 'NY':
+    tax_amount = .089
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+print(result)
