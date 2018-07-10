dc4d7b53f986c7d02acb5056b45fcd243b1f35cc
irsol <irinywka@gmail.com>
2018-07-10 14:07:14 +0200

    Fixed practical quizz.

--- control_flow_lesson_25/control_flow_practice.py
+++ control_flow_lesson_25/control_flow_practice.py
@@ -69,3 +69,34 @@ print(result)
 
 
 # Quiz: Boolean Expressions for Conditions
+
+#You will use a new variable prize to store a prize name if one was won, and
+#then use the truth value of this variable to compose the result message. This
+#will involve two if statements.
+
+#1st conditional statement: update prize to the correct prize name based
+#on points.
+#2nd conditional statement: set result to the correct phrase based on whether
+#prize is evaluated as True or False.
+
+
+points = 174
+
+# establish the default prize value to None
+prize = None
+
+# use the points value to assign prizes to the correct prize names
+if points <= 50:
+    prize = "wooden rabbit"
+elif 151 <= points <= 180:
+    prize = "wafer-thin mint"
+elif 181 <= points <= 200:
+    prize = "penguin"
+
+# use the truth value of prize to assign result to the correct prize
+if prize:
+    result = "Congratulations! You won a {}!".format(prize)
+else:
+    result = "Oh dear, no prize this time."
+
+print(result)
