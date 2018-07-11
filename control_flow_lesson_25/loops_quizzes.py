638375e1c3bca74bf82ca2cb16b536fe0bee01fc
irsol <irinywka@gmail.com>
2018-07-11 13:21:52 +0200

    Added first quizz to the Quiz: Iterating Through Dictionaries topic.

--- control_flow_lesson_25/loops_quizzes.py
+++ control_flow_lesson_25/loops_quizzes.py
@@ -84,3 +84,12 @@ for color in colors:
 print(lower_colors)
 
 
+# Quiz: Iterating Through Dictionaries
+# Quiz 1: Fruit Basket - Task 1
+"""
+You would like to count the number of fruits in your basket. In order to do
+this, you have the following dictionary and list of fruits. Use the dictionary
+and list to count the total number of fruits, but you do not want to count the
+other items in your basket.
+"""
+
