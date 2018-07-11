ef953002f0e1a06511dbd8ec1389be7845d9a27b
irsol <irinywka@gmail.com>
2018-07-11 16:07:14 +0200

    Added solution to the quiz Fruit Basket - Task 3.

--- control_flow_lesson_25/loops_quizzes.py
+++ control_flow_lesson_25/loops_quizzes.py
@@ -171,3 +171,28 @@ for key, value in basket_items.items():
 print(result)
 
 
+# Quiz: Fruit Basket - Task 3
+
+# You would like to count the number of fruits in your basket. 
+# In order to do this, you have the following dictionary and list of
+# fruits.  Use the dictionary and list to count the total number
+# of fruits and not_fruits.
+
+fruit_count, not_fruit_count = 0, 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+#Iterate through the dictionary
+for key, value in basket_items.items():
+
+    #if the key is in the list of fruits, add to fruit_count.
+    if key in fruits:
+        fruit_count = fruit_count + value
+
+    #if the key is not in the list, then add to the not_fruit_count
+    else:
+        not_fruit_count = not_fruit_count + value
+
+print("There are {} fruits and {} not fruits".format(fruit_count, not_fruit_count))
+
+
