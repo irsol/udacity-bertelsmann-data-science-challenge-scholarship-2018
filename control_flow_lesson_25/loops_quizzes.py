e9010e7cbe70df885e1042670cfd3f5c6bc6c24c
irsol <irinywka@gmail.com>
2018-07-11 13:35:39 +0200

    Added more solutions to quizzes.

--- control_flow_lesson_25/loops_quizzes.py
+++ control_flow_lesson_25/loops_quizzes.py
@@ -84,7 +84,8 @@ for color in colors:
 print(lower_colors)
 
 
-# Quiz: Iterating Through Dictionaries
+# Quizzes: Iterating Through Dictionaries
+
 # Quiz 1: Fruit Basket - Task 1
 """
 You would like to count the number of fruits in your basket. In order to do
@@ -92,4 +93,81 @@ this, you have the following dictionary and list of fruits. Use the dictionary
 and list to count the total number of fruits, but you do not want to count the
 other items in your basket.
 """
+
+result = 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+#Iterate through the dictionary
+for key, value in basket_items.items():
+    for item in fruits:
+        #if the key is in the list of fruits, add the value (number of fruits)
+        #to result
+        if item == key:
+            result = result + int(value)
+            #print("Fruits: {} and Result: {}".format(key, value))
+
+print(result)
+
+
+# Quiz: Fruit Basket - Task 2
+"""
+If your solution is robust, you should be able to use it with any dictionary of
+items to count the number of fruits in the basket. Try the loop for each of
+the dictionaries below to make sure it always works.
+"""
+
+#Example 1
+
+result = 0
+basket_items = {'pears': 5, 'grapes': 19, 'kites': 3, 'sandwiches': 8, 'bananas': 4}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+# Your previous solution here
+
+for key, value in basket_items.items():
+    for item in fruits:
+        
+        #if the key is in the list of fruits, add the value (number of fruits) 
+        #to result
+        if item == key:
+            result = result + int(value)
+
+print(result)
+
+#Example 2
+
+result = 0
+basket_items = {'peaches': 5, 'lettuce': 2, 'kites': 3, 'sandwiches': 8, 'pears': 4}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+# Your previous solution here
+
+for key, value in basket_items.items():
+    for item in fruits:
+        
+        #if the key is in the list of fruits, add the value (number of fruits)
+        #to result
+        if item == key:
+            result = result + int(value)
+
+print(result)
+
+#Example 3
+
+result = 0
+basket_items = {'lettuce': 2, 'kites': 3, 'sandwiches': 8, 'pears': 4, 'bears': 10}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+# Your previous solution here
+
+for key, value in basket_items.items():
+    for item in fruits:
+        #if the key is in the list of fruits, add the value (number of fruits)
+        #to result
+        if item == key:
+            result = result + int(value)
+
+print(result)
+
 
