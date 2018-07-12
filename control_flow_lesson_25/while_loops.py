bca21a1c8f270cf8ad5bdbf0a3960c977189c740
irsol <irinywka@gmail.com>
2018-07-12 17:47:33 +0200

    Updated first quizz solution, fixed mistake in print statement inside while loop.

--- control_flow_lesson_25/while_loops.py
+++ control_flow_lesson_25/while_loops.py
@@ -12,7 +12,7 @@ i = 0
 # write your while header line, comparing "i" to the length of the string
 while i < len(print_str):
     #print out the current character from the string
-    print(print_str[i-1])
+    print(print_str[i])
 
     #increment counter variable in the body of the loop
     i = i + 1
