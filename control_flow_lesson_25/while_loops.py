bc50191f5c6be9df6b42ae9bcbce1a207d25d0d7
irsol <irinywka@gmail.com>
2018-07-12 17:10:47 +0200

    Added first practical task solution.

---
+++ control_flow_lesson_25/while_loops.py
@@ -0,0 +1,23 @@
+# while Loops
+
+# 1.Practice: Water Falls
+
+# Print string vertical.
+
+print_str = "Water falls"
+
+# initialize a counting variable "i" to 0
+i = 0
+
+# write your while header line, comparing "i" to the length of the string
+while i < len(print_str):
+    #print out the current character from the string
+    print(print_str[i-1])
+
+    #increment counter variable in the body of the loop
+    i = i + 1
+
+#print(print_str)
+
+
+# 
