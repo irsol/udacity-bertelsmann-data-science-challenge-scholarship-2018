b2297f4d51ebff65be4b7d80cc4f5d59934d9577
irsol <irinywka@gmail.com>
2018-07-28 18:36:53 +0200

    Added solutions to quizz: zip and enumerate.

--- control_flow_lesson_25/loops_quizzes.py
+++ control_flow_lesson_25/loops_quizzes.py
@@ -255,6 +255,20 @@ for num_x, num_y, num_z, letter in zip(x_coord, y_coord, z_coord, labels):
 print(points)
 
 
+# Udacity solution:
+x_coord = [23, 53, 2, -12, 95, 103, 14, -5]
+y_coord = [677, 233, 405, 433, 905, 376, 432, 445]
+z_coord = [4, 16, -6, -42, 3, -6, 23, -1]
+labels = ["F", "J", "A", "Q", "Y", "B", "W", "X"]
+
+points = []
+for point in zip(labels, x_coord, y_coord, z_coord):
+    points.append("{}: {}, {}, {}".format(*point))
+
+for point in points:
+    print(point)
+
+
 # Quiz 2: zip() and enumerate()
 #
 # Zip Lists to a Dictionary
@@ -262,7 +276,7 @@ print(points)
 cast_names = ["Barney", "Robin", "Ted", "Lily", "Marshall"]
 cast_heights = [72, 68, 72, 66, 76]
 
-cast = dict(zip(cast_names, cast_heights)) # replace with your code
+cast = dict(zip(cast_names, cast_heights)) 
 
 print(cast)
 
@@ -294,4 +308,22 @@ cast = (("Barney", 72), ("Robin", 68), ("Ted", 72), ("Lily", 66), ("Marshall", 7
 names, heights = zip(*cast)
 
 print(names)
-print(heights)
+print(heights
+
+
+# Quiz 5: Quiz: Enumerate
+#
+# Use enumerate to modify the cast list so that each element contains the name
+# followed by the character's corresponding height. For example, the first
+# element of cast should change from "Barney Stinson" to "Barney Stinson 72".
+
+cast = ["Barney Stinson", "Robin Scherbatsky", "Ted Mosby", "Lily Aldrin", "Marshall Eriksen"]
+heights = [72, 68, 72, 66, 76]
+
+for index, height in enumerate(heights):
+    s = "{} {}".format(cast[index], height)
+    cast[index] = s
+
+print(cast)
+
+
