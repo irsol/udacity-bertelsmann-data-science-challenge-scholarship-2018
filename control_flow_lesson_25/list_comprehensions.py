1f875e3f2f5d8b0c25e70139e96b4d7d33a27517
irsol <irinywka@gmail.com>
2018-07-29 17:57:40 +0200

    Added list comprehensions solutions.

--- control_flow_lesson_25/list_comprehensions.py
+++ control_flow_lesson_25/list_comprehensions.py
@@ -14,3 +14,35 @@ print(first_names)
 # Use a list comprehension to create a list multiples_3 containing the first
 # 20 multiples of 3.
 
+multiples_3 = [ x for x in range(3, 60+1)  if x % 3 == 0]
+print(multiples_3)
+
+# Second solution:
+
+multiples_3 = [x * 3 for x in range(1, 21)]
+print(multiples_3)
+
+
+# Quiz: Filter Names by Scores
+# Use a list comprehension to create a list of names passed that only include
+# those that scored at least 65.
+#
+
+
+scores = {
+             "Rick Sanchez": 70,
+             "Morty Smith": 35,
+             "Summer Smith": 82,
+             "Jerry Smith": 23,
+             "Beth Smith": 98
+          }
+
+
+passed = [key for key, value in scores.items() if value >= 65]
+print(passed)
+
+
+# Udacity solution:
+
+passed = [name for name, score in scores.items() if score >= 65]
+print(passed)
