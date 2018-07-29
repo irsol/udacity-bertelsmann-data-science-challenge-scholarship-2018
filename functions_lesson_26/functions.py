005aa9cca0c342eb917653c38d796ff048e214f1
irsol <irinywka@gmail.com>
2018-07-29 19:39:50 +0200

    Added solutions to quizzes.

--- functions_lesson_26/functions.py
+++ functions_lesson_26/functions.py
@@ -20,3 +20,30 @@ print('\nCalling add_ten...')
 return_value_2 = add_ten(10)
 print('Done calling')
 print('This function returned: {}'.format(return_value_2))
+
+
+# Quiz: Population Density Function
+
+def population_density(population, land_area):
+    return population / land_area
+
+# test cases for your function
+test1 = population_density(10, 1)
+expected_result1 = 10
+print("expected result: {}, actual result: {}".format(expected_result1, test1))
+
+test2 = population_density(864816, 121.4)
+expected_result2 = 7123.6902801
+print("expected result: {}, actual result: {}".format(expected_result2, test2))
+
+
+# Quiz: readable_timedelta
+
+def readable_timedelta(days):
+    week = days // 7
+    # % to get the number of days that remain
+    day = days % 7
+    return "{} week(s) and {} day(s).".format(week, day)
+
+print(readable_timedelta(6))
+
