1450db4ea8a247a85d2669c7cb5d4af7e5f5561b
irsol <irinywka@gmail.com>
2018-08-03 13:21:23 +0200

    Added more solutions to case quiz.

--- aggregations_lesson_30/cas_statement.sql
+++ aggregations_lesson_30/cas_statement.sql
@@ -39,6 +39,18 @@ WHERE o.occurred_at BETWEEN '2016-01-01' and '2017-12-31'
 GROUP BY a.name, year
 ORDER BY 3 DESC;
 
+# 2. Udacity solution
+
+SELECT a.name, SUM(total_amt_usd) total_spent, 
+     CASE WHEN SUM(total_amt_usd) > 200000 THEN 'top'
+     WHEN  SUM(total_amt_usd) > 100000 THEN 'middle'
+     ELSE 'low' END AS customer_level
+FROM orders o
+JOIN accounts a
+ON o.account_id = a.id
+WHERE occurred_at > '2015-12-31' 
+GROUP BY 1
+ORDER BY 2 DESC;
 
 # 3. Quiz: CASE
 
@@ -61,7 +73,12 @@ ORDER BY 2 DESC;
 # 4. Quiz: CASE
 
 /*
-
+The previous didn't account for the middle, nor the dollar amount associated with the sales. Management
+decides they want to see these characteristics represented as well. We would like to identify top performing
+sales reps, which are sales reps associated with more than 200 orders or more than 750000 in total sales.
+The middle group has any rep with more than 150 orders or 500000 in sales. Create a table with the sales rep name,
+the total number of orders, total sales across all orders, and a column with top, middle, or low depending on this
+criteria. Place the top sales people based on dollar amount of sales first in your final table.
 */
 
 SELECT s.name,
@@ -75,3 +92,17 @@ JOIN accounts a on o.account_id = a.id
 JOIN sales_reps s on a.sales_rep_id = s.id
 GROUP BY s.name
 ORDER BY sales_level DESC; 
+
+# 4. Udacity Solution
+
+SELECT s.name, COUNT(*), SUM(o.total_amt_usd) total_spent, 
+     CASE WHEN COUNT(*) > 200 OR SUM(o.total_amt_usd) > 750000 THEN 'top'
+     WHEN COUNT(*) > 150 OR SUM(o.total_amt_usd) > 500000 THEN 'middle'
+     ELSE 'low' END AS sales_rep_level
+FROM orders o
+JOIN accounts a
+ON o.account_id = a.id 
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+GROUP BY s.name
+ORDER BY 3 DESC;
