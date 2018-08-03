73aa9adc00f2b858a38bda29d8c1e3f7aec3bbb6
irsol <irinywka@gmail.com>
2018-08-03 13:14:51 +0200

    Added solution to CASE quizzes.

---
+++ aggregations_lesson_30/cas_statement.sql
@@ -0,0 +1,77 @@
+# 1. Quiz: CASE
+
+/*
+We would like to understand 3 different levels of customers based on the amount associated with their purchases.
+The top branch includes anyone with a Lifetime Value (total sales of all orders) greater than 200,000 usd.
+The second branch is between 200,000 and 100,000 usd. The lowest branch is anyone under 100,000 usd.
+Provide a table that includes the level associated with each account. You should provide the account name, 
+the total sales of all orders for the customer, and the level. Order with the top spending customers listed first.
+*/
+
+SELECT a.name,
+	   SUM(o.total_amt_usd),
+       CASE WHEN SUM(o.total_amt_usd) > 200000 THEN 'Over 200,000'
+       		WHEN SUM(o.total_amt_usd) > 100000 THEN 'Over 100,000 '
+       		ELSE 'Less 100,000' END as total_level
+FROM orders o
+JOIN accounts a on a.id = o.account_id
+GROUP BY a.name
+ORDER BY 2 DESC; 
+
+
+# 2. Quiz: CASE
+
+/* 
+We would now like to perform a similar calculation to the first, but we want to obtain the total amount spent
+by customers only in 2016 and 2017. Keep the same levels as in the previous question. Order with the top spending
+customers listed first.
+*/
+
+SELECT DATE_TRUNC('year', o.occurred_at) as year,
+	   a.name,
+	   SUM(o.total_amt_usd),
+       CASE WHEN SUM(o.total_amt_usd) > 200000 THEN 'Over 200,000'
+       		WHEN SUM(o.total_amt_usd) > 100000 THEN 'Over 100,000 '
+       		ELSE 'Less 100,000' END as total_level
+FROM orders o
+JOIN accounts a on a.id = o.account_id
+WHERE o.occurred_at BETWEEN '2016-01-01' and '2017-12-31'
+GROUP BY a.name, year
+ORDER BY 3 DESC;
+
+
+# 3. Quiz: CASE
+
+/*
+We would like to identify top performing sales reps, which are sales reps associated with more than 200 orders.
+Create a table with the sales rep name, the total number of orders, and a column with top or not depending on if
+they have more than 200 orders. Place the top sales people first in your final table.
+*/
+
+SELECT s.name,
+	   COUNT(*) as number_of_orders,
+       CASE WHEN COUNT(*) > 200 THEN 'top'
+       ELSE 'not' END as sales_level
+FROM orders o
+JOIN accounts a on o.account_id = a.id
+JOIN sales_reps s on a.sales_rep_id = s.id
+GROUP BY s.name
+ORDER BY 2 DESC;
+
+# 4. Quiz: CASE
+
+/*
+
+*/
+
+SELECT s.name,
+	   COUNT(*) as number_of_orders,
+       SUM(total_amt_usd) as total_usd,
+       CASE WHEN COUNT(*) > 200 and SUM(total_amt_usd) > 750000 THEN 'top'
+       		WHEN COUNT(*) > 150 and SUM(total_amt_usd) > 500000 THEN 'middle'
+       	    ELSE 'not' END as sales_level
+FROM orders o
+JOIN accounts a on o.account_id = a.id
+JOIN sales_reps s on a.sales_rep_id = s.id
+GROUP BY s.name
+ORDER BY sales_level DESC; 
