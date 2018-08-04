18a377498bc49a8ff98e5f80112f8fed0c24b753
irsol <irinywka@gmail.com>
2018-08-04 12:34:44 +0200

    Added sql tasks.

--- sql_subqueries_temporary_table_lesson31/subqueries_tasks.sql
+++ sql_subqueries_temporary_table_lesson31/subqueries_tasks.sql
@@ -59,3 +59,28 @@ FROM orders
 WHERE DATE_TRUNC('month', occurred_at) =
 (SELECT DATE_TRUNC('month', MIN(occurred_at)) AS min
 FROM orders);
+
+
+# QUIZ: Subquery Mania
+# 1. Provide the name of the sales_rep in each region with the largest amount of total_amt_usd sales.
+
+SELECT s.name, s.region_id, MAX(o.total_amt_usd) as max_total
+FROM sales_reps s
+JOIN accounts a ON s.id = a.sales_rep_id
+JOIN orders o ON a.id = o.account_id
+GROUP BY s.name, s.region_id
+ORDER BY max_total DESC;
+
+# 2. For the region with the largest (sum) of sales total_amt_usd, how many total (count) orders were placed? 
+
+SELECT s.name, r.name, SUM(o.total_amt_usd) as total_amt_usd, COUNT(total) total_orders
+FROM region r
+JOIN sales_reps s ON r.id = s.region_id
+JOIN accounts a ON s.id = a.sales_rep_id
+JOIN orders o ON a.id = o.account_id
+GROUP BY s.name, r.name
+ORDER BY total_amt_usd DESC;
+
+# 3. For the name of the account that purchased the most (in total over their lifetime as a customer)
+# standard_qty paper, how many accounts still had more in total purchases? 
+
